6793407eb66a3e2eee4546b7a1b1684fcbad913b
Javier Goizueta <jgoizueta@gmail.com>
2015-05-10 17:00:32 +0200

    Use MetaCodec for the packed metadata

--- lib/dicom_pack.rb
+++ lib/dicom_pack.rb
@@ -3,6 +3,43 @@ require 'dicom'
 require 'modalsettings'
 require 'sys_cmd'
 require 'narray'
+require_relative 'meta_codec'
+
+# TODO: require known SOP Class: 1.2.840.10008.5.1.4.1.1.2
+# (in tag 0002,0002, Media Storage SOP Class UID)
+# And Transfer Syntax 0002,0010 1.2.840.10008.1.2:
+#   Implicit VR Little Endian: Default Transfer Syntax for DICOM
+#
+
+# TODO: option for pack to restore DICOM files;
+# we need to adjust metadata elements (which refer to first slice)
+# to each slice.
+# elements that vary from slice to slice but whose variation probably doesn't matter:
+#   0008,0033     Content Time
+# elements that vary and should be removed:
+#   0018,1151     X-Ray Tube Current
+#   0018,1152     Exposure
+# elements that should be adjusted:
+#   0020,0013     Instance Number # increment by 1 for each slice
+#   0020,0032     Image Position (Patient) # should be computed from additional metadata (dz)
+#   0020,1041     Slice Location  # should be computed from additional metadata (dz)
+# elements that may need adjusting depending on value restoration method:
+#   0028,0100     Bits Allocated
+#   0028,0101     Bits Stored
+#   0028,0102     High Bit
+#   0028,0103     Pixel Representation
+#   0028,1050     Window Center
+#   0028,1051     Window Width
+#   0028,1052     Rescale Intercept
+#   0028,1053     Rescale Slope
+#   0028,1054     Rescale Type
+# also, these element should be removed (because Pixel data is set by assigning an image)
+#   7FE0,0000     Group Length
+#   7FE0,0010     Pixel Data
+# other varying elements that need further study: (vary in some studies)
+#   0002,0000     File Meta Information Group Length # drop this and 0002,0001?
+#   0002,0003     Media Storage SOP Instance UID
+#   0008,0018     SOP Instance UID
 
 class DicomPack
 
@@ -392,13 +429,23 @@ class DicomPack
     end
     metadata.nz = n
     metadata.dz = (last_z - first_z)/(n-1)
+    if options[:dicom_metadata]
+      metadata_file = File.join(pack_fir, 'ffmetadata')
+      # TODO: filter-out elements to be ignored
+      meta_codec.write_metadata(DICOM::DObject.read(dicom_files.first), metadata_file, metadata.to_h)
+    end
     ffmpeg = SysCmd.command('ffmpeg', @ffmpeg_options) do
       option '-start_number', start_number
       option '-i', name_pattern
       option '-vcodec', 'mjpeg'
       option '-q:v', '2.0'
-      metadata.each do |key, value|
-        option '-metadata', key, equal_value: value
+      if metadata_file
+        option '-i', metadata_file
+        option '-map_metadata', 1
+      else
+        metadata.each do |key, value|
+          option '-metadata', "dicom_#{key}", equal_value: value
+        end
       end
       file output_name
     end
@@ -431,14 +478,18 @@ class DicomPack
     ffmpeg.run
     check_command ffmpeg
 
-    metadata = File.read(metadata_file).lines[1..-1].map { |line|
-      key, value = line.strip.split('=')
-      key = key.downcase.to_sym
+    dicom_elements, metadata = meta_codec.read_metadata(metadata_file)
+    metadata = Hash[metadata.to_a.map { |key, value|
+      key = key.to_s.downcase.to_sym
       trans = METADATA_TYPES[key]
       value = value.send(trans) if trans
       [key, value]
-    }
-    metadata = Hash[*metadata.flatten]
+    }]
+    # TODO:
+    # now if dicom_elements are present and are going to be used,
+    # we need to adjust slice-varying elements and associate to
+    # each slice
+
     metadata_yaml = File.join(unpack_dir, 'metadata.yml')
     File.open(metadata_yaml, 'w') do |yaml|
       yaml.write metadata.to_yaml
@@ -446,6 +497,10 @@ class DicomPack
   end
 
   private
+
+  def meta_codec
+    MetaCodec.new
+  end
 
   # def optimize_dynamic_range(data, output_min, output_max, options = {})
   #   minimum, maximum = options[:range]
@@ -468,7 +523,8 @@ class DicomPack
 
   METADATA_TYPES = {
     dx: :to_f, dy: :to_f, dz: :to_f,
-    nx: :to_i, ny: :to_i, nz: :to_i
+    nx: :to_i, ny: :to_i, nz: :to_i,
+    max: :to_i, min: :to_i
   }
 
   def check_command(command)
